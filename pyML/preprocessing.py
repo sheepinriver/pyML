6d2b86f0ad3ab690d3c83b2b0577e2280f6d6f27
Henson He <sheepinriver@hotmail.com>
2020-05-01 22:17:55 +0800

    # MinMaxScaler: add class MinMaxScaler

--- pyML/preprocessing.py
+++ pyML/preprocessing.py
@@ -25,3 +25,30 @@ class StandardScaler:
         return (X - self._mean) / self._scale
 
 
+class MinMaxScaler:
+
+    def __init__(self):
+        self._min = None
+        self._max = None
+
+    def fit(self, X):
+        """根据训练数据集X获取最大和最小值"""
+
+        assert X.ndim == 2, 'X must be 2 dimension'
+
+        self._min = np.min(X, axis=0)
+        self._max = np.max(X, axis=0)
+
+        return self
+
+    def transform(self, X):
+        """将传入的X根据self._min和self._min进行最值归一化"""
+        assert X.ndim == 2, 'X must be 2 dimension'
+        assert self._min is not None and self._max is not None, \
+            'must fit before transform'
+
+        return (X - self._min) / (self._max - self._min)
+
+
+
+
